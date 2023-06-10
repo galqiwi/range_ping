702a733e839e529816dee6f17c6a57458f5447e1
galqiwi <galqiwi@yandex-team.ru>
2023-06-11 02:27:52 +0300

    mask iterator + cmd

--- cmd/range_ping/main.go
+++ cmd/range_ping/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/galqiwi/range_ping/internal/icmp_client"
+	"github.com/galqiwi/range_ping/internal/mask_iterator"
 	"log"
 	"net"
+	"strings"
 	"sync"
 	"time"
 )
@@ -15,19 +17,35 @@ func timeIt(f func()) {
 	fmt.Println(time.Since(begin))
 }
 
+func worker(client icmp_client.ICMPClient, ips chan net.IP) {
+	for ip := range ips {
+		for {
+			msg, err := client.SendRequest(ip)
+			if err != nil && strings.Contains(err.Error(), "no buffer space available") {
+				time.Sleep(time.Second)
+				continue
+			}
+			fmt.Println(ip, msg, err)
+		}
+	}
+}
+
 func Main() error {
+	ips, err := mask_iterator.IPGenerator("1.1.1.1/8")
+	if err != nil {
+		return err
+	}
+
 	client, err := icmp_client.NewICMPClient(time.Second * 3)
 	if err != nil {
 		return err
 	}
 
 	var wg sync.WaitGroup
-	for i := 0; i < 256; i += 1 {
-		i := i
+	for i := 0; i < 5000; i += 1 {
 		wg.Add(1)
 		go func() {
-			resp, err := client.SendRequest(net.IP{1, 1, 1, byte(i)})
-			fmt.Println(i, resp, err)
+			worker(client, ips)
 			wg.Done()
 		}()
 	}
---
+++ internal/mask_iterator/mask_iterator.go
@@ -0,0 +1,33 @@
+package mask_iterator
+
+import (
+	"net"
+)
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+func IPGenerator(mask string) (chan net.IP, error) {
+	ip, ipNet, err := net.ParseCIDR(mask)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make(chan net.IP)
+
+	go func() {
+		for ip := ip.Mask(ipNet.Mask); ipNet.Contains(ip); inc(ip) {
+			outputIP := make(net.IP, 4)
+			copy(outputIP, ip)
+			output <- outputIP
+		}
+	}()
+
+	return output, nil
+}
